db799ccdcac1ead015279404c15836d7c7bfa695
Andrew Garde Joia <drewdrewthis@gmail.com>
2017-08-19 20:32:19 -0400

    Reorganize directories && create Associations

--- micro-reddit/app/models/comment.rb
+++
@@ -1,9 +0,0 @@
-class Comment < ApplicationRecord
-  validates :post_id, :user_id, presence: true
-  validates :body,
-    presence: true,
-    length: { maximum: 255 }
-
-  belongs_to :user
-  belongs_to :post
-end
--- micro-reddit/app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ApplicationRecord
-  validates :url, presence: true
-  validates :user_id, presence: true
-
-  belongs_to :user
-  has_many :comments
-end
