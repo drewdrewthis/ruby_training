733ad4d89820e9b6236c94831c6708b224cd4631
Andrew Joia <drewdrewthis@gmail.com>
2017-08-23 11:39:58 -0400

    Add annotations to models

--- RailsTutorials/private-events/app/models/user.rb
+++ RailsTutorials/private-events/app/models/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  username   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username,
     presence: true,
--- RailsTutorials/private-events/test/models/event_attendee_test.rb
+++ RailsTutorials/private-events/test/models/event_attendee_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: event_attendees
+#
+#  id                :integer          not null, primary key
+#  attendee_id       :integer
+#  attended_event_id :integer
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+
 require 'test_helper'
 
 class EventAttendeeTest < ActiveSupport::TestCase
